c399a2aa04c53023d5eaeb29c887b260140b37f0
LeoNardoRR <ribeiroleonardoti@gmail.com>
2023-01-14 11:23:35 -0300

    Incluindo consoles

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("Rodando o sistema de gerencimento de jogos")
+console.log("A aplicação esta se conectando ao banco de dados")
+
+// mais um console de log de teste //
+
+console.log("Fulano fez esse commit sem você na aula")
